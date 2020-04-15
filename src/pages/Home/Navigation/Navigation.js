06f08d14b84637d844df594f433fce757b0d8f62
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-15 01:06:39 +0100

    Added navigation component

--- src/pages/Home/Navigation/Navigation.js
+++ src/pages/Home/Navigation/Navigation.js
@@ -5,29 +5,35 @@ const Navigation = () => {
   return (
     <div className="codesyrup-navigation">
       <div className="codesyrup-compact">
-        <div className="grid-md-13">
-          <div></div>
+        <div className="grid-md-1">
           <div>
-            <ul>
-              <li>
-                <a href="https://codesyrup.io">HTML</a>
-              </li>
-              <li>
-                <a href="https://codesyrup.io">CSS</a>
-              </li>
-              <li>
-                <a href="https://codesyrup.io">JavaScript</a>
-              </li>
-              <li>
-                <a href="https://codesyrup.io">React.js</a>
-              </li>
-              <li>
-                <a href="https://codesyrup.io">PHP</a>
-              </li>
-              <li>
-                <a href="https://codesyrup.io">Laravel</a>
-              </li>
-            </ul>
+            <div className="grid-md-1">
+              <div>
+                <ul>
+                  <li>
+                    <a href="https://codesyrup.io">HTML</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">CSS</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">JavaScript</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">React</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">Node.js</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">PHP</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">Laravel</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
