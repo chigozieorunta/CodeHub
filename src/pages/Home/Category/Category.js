2e70eeca33e819b6fbc009cf010eaab8de4c0358
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-17 07:59:50 +0100

    Modified category component to make async call to API

---
+++ src/pages/Home/Category/Category.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import CourseBox from "../CourseBox/CourseBox.js";
+import "./Category.css";
+
+const h1 = {
+  marginBottom: "0.1em",
+};
+
+class Category extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+      res.json().then((json) => {
+        this.setState({
+          courses: json,
+          isLoaded: true,
+        });
+      })
+    );
+  }
+
+  render() {
+    const { title, excerpt, total } = this.props;
+    return (
+      <section className="section-md">
+        <Container fluid>
+          <div className="site-wrapper">
+            <Row className="row-col-spacer">
+              <Col lg={3}>
+                <small>{total} COURSES</small>
+                <h1 className="site-caption" style={h1}>
+                  {title}
+                </h1>
+                <h3 className="site-excerpt">{excerpt}</h3>
+                <a className="site-button" href="https://codesyrup.io">
+                  Explore
+                </a>
+              </Col>
+              <Col lg={9}>
+                <Row>
+                  {this.state.courses.map((course) => (
+                    <>
+                      <CourseBox {...course} />
+                    </>
+                  ))}
+                </Row>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </section>
+    );
+  }
+}
+
+export default Category;
