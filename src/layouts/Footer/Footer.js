5a8a8f19381e3b0caae87473492bcecbcf8bb66c
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-17 07:59:24 +0100

    Added new footer layout component

---
+++ src/layouts/Footer/Footer.js
@@ -0,0 +1,122 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="section-md">
+      <div className="codesyrup-compact">
+        <div className="grid-md-12">
+          <div>
+            <h4>CodeSyrup.io</h4>
+            <ul className="about">
+              <li>
+                Building the best developers and engineers around the world with
+                simple web technologies.
+              </li>
+              <li>
+                Our goal is to provide beginners with the relevant soft skills
+                training presented in a way that would make them passionate
+                about coding and software development.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div className="grid-md-4">
+              <div>
+                <h4>Courses</h4>
+                <ul>
+                  <li>
+                    <a href="https://codesyrup.io">HTML</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">CSS</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">JavaScript</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">React</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">Node.js</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">PHP</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">Laravel</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h4>Account</h4>
+                <ul>
+                  <li>
+                    <a href="https://codesyrup.io">Sign Up</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">Login</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">How It Works</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">FAQs</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h4>Social</h4>
+                <ul>
+                  <li>
+                    <a href="https://codesyrup.io">Facebook</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">Twitter</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">Instagram</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">YouTube</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">LinkedIn</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h4>Courses</h4>
+                <ul>
+                  <li>
+                    <a href="https://codesyrup.io">HTML</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">CSS</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">JavaScript</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">React</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">Node.js</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">PHP</a>
+                  </li>
+                  <li>
+                    <a href="https://codesyrup.io">Laravel</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
