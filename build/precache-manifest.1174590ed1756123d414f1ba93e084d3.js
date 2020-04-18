9d5ae72ec3cdec47e74e99fd70dc8ca97122898b
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-18 11:45:46 +0100

    Added build directory

---
+++ build/precache-manifest.1174590ed1756123d414f1ba93e084d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42476ac50bbdb5e321a233c2782eb096",
+    "url": "/index.html"
+  },
+  {
+    "revision": "457e19925f4e066d21f7",
+    "url": "/static/css/main.ae286deb.chunk.css"
+  },
+  {
+    "revision": "a28c44bf6f58bb5d7164",
+    "url": "/static/js/2.72944a73.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.72944a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457e19925f4e066d21f7",
+    "url": "/static/js/main.7d062bc9.chunk.js"
+  },
+  {
+    "revision": "394ffc908e47ab445a31",
+    "url": "/static/js/runtime-main.b59ba584.js"
+  },
+  {
+    "revision": "e4c16a6119beb2ac8e0ae1f74671dcc8",
+    "url": "/static/media/background1.e4c16a61.png"
+  },
+  {
+    "revision": "fd826bb8fa5ebfde6e2c98a4e7672ca7",
+    "url": "/static/media/html1.fd826bb8.png"
+  }
+]);
