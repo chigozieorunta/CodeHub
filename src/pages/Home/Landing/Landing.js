40b34c8f89f763c6dcae867f0b14f087205db0ec
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-18 14:14:57 +0100

    modified excerpt

--- src/pages/Home/Landing/Landing.js
+++ src/pages/Home/Landing/Landing.js
@@ -12,7 +12,7 @@ const Landing = () => {
             <Row>
               <Col md={12} lg={6} className="site-landing-text">
                 <h1 className="site-caption">CodeHub.pro</h1>
-                <h3 className="site-excerpt d-none d-sm-block">
+                <h3 className="site-excerpt">
                   Building the best developers and engineers around the world
                   with simple web technologies…
                 </h3>
