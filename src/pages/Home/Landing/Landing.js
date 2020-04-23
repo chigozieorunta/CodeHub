9da99b7ed2d19c446957ca091c510e6365eace24
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-23 01:55:09 +0100

    rebuilt component using new CenterScreen component

--- src/pages/Home/Landing/Landing.js
+++ src/pages/Home/Landing/Landing.js
@@ -1,31 +1,24 @@
 import React from "react";
-import Navigation from "../Navigation/Navigation";
-import { Container, Row, Col } from "react-bootstrap";
+import CenterScreen from "../../../components/CenterScreen/CenterScreen";
+import { Row, Col } from "react-bootstrap";
 import "./Landing.css";
 
 const Landing = () => {
   return (
-    <section className="site-fs" id="landing">
-      <div className="site-fs-child">
-        <Container fluid>
-          <div className="site-wrapper">
-            <Row>
-              <Col md={12} lg={6} className="site-landing-text">
-                <h1 className="site-caption">CodeHub.pro</h1>
-                <h3 className="site-excerpt">
-                  Building the best developers and engineers around the world
-                  with simple web technologies…
-                </h3>
-                <a className="site-button" href="https://codehub.pro">
-                  Get Started
-                </a>
-              </Col>
-            </Row>
-          </div>
-        </Container>
-      </div>
-      <Navigation />
-    </section>
+    <CenterScreen id="landing">
+      <Row>
+        <Col md={12} lg={6} className="site-landing-text">
+          <h1 className="site-caption">CodeHub.pro</h1>
+          <h3 className="site-excerpt">
+            Building the best developers and engineers around the world with
+            simple web technologies…
+          </h3>
+          <a className="site-button" href="https://codehub.pro">
+            Get Started
+          </a>
+        </Col>
+      </Row>
+    </CenterScreen>
   );
 };
 
