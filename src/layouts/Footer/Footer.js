142da43512cba4d99d5f5a1e5e85648e3c222c05
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-23 01:54:30 +0100

    rebuilt component afresh

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./Footer.css";
 
-const Footer = () => <footer></footer>;
+const Footer = () => (
+  <footer className="section-md">
+    <Container fluid>
+      <Row className="text-center">
+        <Col lg={{ span: 8, offset: 2 }}>
+          CodeHub.pro is an online web academy focused on building the best
+          developers and engineers around the world with simple web
+          technologies… <br />
+          Copyright &copy; 2020 CodeHub.pro. All Rights Reserved.
+        </Col>
+      </Row>
+    </Container>
+  </footer>
+);
 
 export default Footer;
