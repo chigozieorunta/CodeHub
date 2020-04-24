23a14c263204c9988cb934db849c3f6b1bab2941
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-24 19:22:29 +0100

    changed TopNavigation to Navigation component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import Landing from "./Landing/Landing";
 import Courses from "./Courses/Courses";
 import Footer from "../../layouts/Footer/Footer";
-import TopNavigation from "./TopNavigation/TopNavigation";
+import Navigation from "../../components/Navigation/Navigation";
 
 const Home = () => {
   return (
     <>
-      <TopNavigation />
+      <Navigation />
       <Landing />
       <Courses />
       <Footer />
