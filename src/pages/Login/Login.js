8193d32d5c08729046e1e3b349daa08c9920ed0a
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-25 21:04:51 +0100

    added login component

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CenterScreen from "../../components/CenterScreen/CenterScreen";
+
+const Login = () => {
+  return (
+    <CenterScreen>
+      <Row>
+        <Col lg={{ span: 4, offset: 4 }}>Login...</Col>
+      </Row>
+    </CenterScreen>
+  );
+};
+
+export default Login;
