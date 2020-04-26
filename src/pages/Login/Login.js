b9f801fb85f9ceb2e4f253f74a03977abee69e7a
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-26 22:28:30 +0100

    added login form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,18 @@
 import React from "react";
 import CenterScreen from "../../components/CenterScreen/CenterScreen";
+import { Row, Col } from "react-bootstrap";
 
 const Login = () => {
   return (
-    <CenterScreen>
+    <CenterScreen id="landing">
       <Row>
-        <Col lg={{ span: 4, offset: 4 }}>Login...</Col>
+        <Col
+          lg={{ span: 4, offset: 4 }}
+          className="site-landing-text text-center"
+        >
+          <h1 className="site-caption">Login</h1>
+          <h5 className="site-footnote">Enter your username and password...</h5>
+        </Col>
       </Row>
     </CenterScreen>
   );
