148c97c06c986cace20b1bffec4013b82b562558
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-26 22:29:03 +0100

    added Switch, Router, Route named exports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Switch, Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import Home from "./pages/Home/";
+import Login from "./pages/Login/Login";
 import "../src/styles/App.css";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Router history={createBrowserHistory()}>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/login" exact component={Login}></Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
