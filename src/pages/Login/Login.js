51b39a3368b88bcf5409233f95538c33d19311ad
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-27 19:29:52 +0100

    modified login component

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,20 +1,38 @@
 import React from "react";
 import CenterScreen from "../../components/CenterScreen/CenterScreen";
-import { Row, Col } from "react-bootstrap";
+import Navigation from "../../components/Navigation/Navigation";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <CenterScreen id="landing">
-      <Row>
-        <Col
-          lg={{ span: 4, offset: 4 }}
-          className="site-landing-text text-center"
-        >
-          <h1 className="site-caption">Login</h1>
-          <h5 className="site-footnote">Enter your username and password...</h5>
-        </Col>
-      </Row>
-    </CenterScreen>
+    <>
+      <Navigation />
+      <CenterScreen id="landing">
+        <Row>
+          <Col lg={{ span: 4, offset: 4 }} className="site-landing-text">
+            <h1 className="site-caption">Login</h1>
+            <Form>
+              <Form.Group>
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="you@email.com"
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="*****"
+                ></Form.Control>
+              </Form.Group>
+              <Button className="site-button">Login</Button>
+            </Form>
+          </Col>
+        </Row>
+      </CenterScreen>
+    </>
   );
 };
 
