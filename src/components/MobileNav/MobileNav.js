a8068e70d56d6d7dc9e80e52d3ceddbd4517f2ad
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-28 23:19:28 +0100

    created MobileNav component

---
+++ src/components/MobileNav/MobileNav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MobileNav.css";
+
+const MobileNav = () => {
+  return (
+    <section id="site-mobile-nav">
+      <button>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </section>
+  );
+};
+
+export default MobileNav;
