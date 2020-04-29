e494f618afa4fbbf2857f3f3e9b507a8fca2276c
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-29 20:57:32 +0100

    modified slideOut event handler for component

--- src/components/MobileNav/MobileNav.js
+++ src/components/MobileNav/MobileNav.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MobileNav.css";
 
+let state = false;
+
+const hiddenState = {
+  left: "-80%",
+};
+
+const visibleState = {
+  left: "0",
+};
+
 const MobileNav = () => {
+  const [mobileNavState, setMobileNavState] = useState(hiddenState);
+
+  const slideOut = () => {
+    !state ? setMobileNavState(visibleState) : setMobileNavState(hiddenState);
+    state = !state;
+  };
+
   return (
-    <section id="site-mobile-nav">
-      <button>
+    <section>
+      <button id="site-mobile-toggler" onClick={slideOut}>
         <span></span>
         <span></span>
         <span></span>
       </button>
+      <ul id="site-mobile-nav" style={mobileNavState}>
+        <li>
+          <a href="http://codehub.pro">Home</a>
+        </li>
+        <li>
+          <a href="http://codehub.pro/courses">Courses</a>
+        </li>
+        <li>
+          <a href="http://codehub.pro/sign-up">Sign Up</a>
+        </li>
+        <li>
+          <a href="http://codehub.pro/login">Login</a>
+        </li>
+        <li>
+          <a href="http://codehub.pro/sign-up">How It Works</a>
+        </li>
+        <li>
+          <a href="http://codehub.pro/sign-up">FAQs</a>
+        </li>
+      </ul>
     </section>
   );
 };
