3c1ba0256924c7b47895d499865d0e8a3a54923b
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-30 13:42:22 +0100

    added mobile nav to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,10 +3,12 @@ import Landing from "./Landing/Landing";
 import Courses from "./Courses/Courses";
 import Footer from "../../layouts/Footer/Footer";
 import Navigation from "../../components/Navigation/Navigation";
+import MobileNav from "../../components/MobileNav/MobileNav";
 
 const Home = () => {
   return (
     <>
+      <MobileNav />
       <Navigation />
       <Landing />
       <Courses />
