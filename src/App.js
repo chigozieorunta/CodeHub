2fba5345e9f5e6373d2ca08490c997da668c3d27
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-30 14:58:02 +0100

    modified component names and paths

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Switch, Router, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
+
 import Home from "./pages/Home/";
+import Courses from "./pages/Courses/";
 import Login from "./pages/Login/Login";
+
 import "../src/styles/App.css";
+import "../src/styles/BootStrap-Spacer.min.css";
 
 const App = () => {
   return (
@@ -11,6 +15,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={Home}></Route>
         <Route path="/login" exact component={Login}></Route>
+        <Route path="/courses" exact component={Courses}></Route>
       </Switch>
     </Router>
   );
