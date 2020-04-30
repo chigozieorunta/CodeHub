6eed340bd4001a1d00d51ce94b2577e96c8325f7
chigozieorunta <chigozieorunta@yahoo.com>
2020-04-30 15:53:50 +0100

    refactored CourseNavigation component

--- src/components/CourseNavigation/CourseNavigation.js
+++ src/components/CourseNavigation/CourseNavigation.js
@@ -1,78 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import { Link } from "react-router-dom";
 
 import "./CourseNavigation.css";
 
-const navBgTop = {
-  backgroundColor: "transparent",
-  transition: "all 0.3s",
-  left: 0,
-  bottom: 0,
-  top: "auto",
-  position: "absolute",
-  padding: "2em 0",
-  width: "100%",
-};
-
-const navBgScroll = {
-  backgroundColor: "#2a2a2a",
-  transition: "all 0.3s",
-  left: 0,
-  bottom: "auto",
-  top: 0,
-  position: "fixed",
-  padding: "2em 0",
-  width: "100%",
-};
+const links = [
+  {
+    name: "HTML",
+    slug: "html",
+  },
+  {
+    name: "CSS",
+    slug: "css",
+  },
+  {
+    name: "JavaScript",
+    slug: "javascript",
+  },
+  {
+    name: "React",
+    slug: "react",
+  },
+  {
+    name: "Node.js",
+    slug: "nodejs",
+  },
+  {
+    name: "PHP",
+    slug: "php",
+  },
+  {
+    name: "Laravel",
+    slug: "laravel",
+  },
+];
 
 const CourseNavigation = () => {
-  const [navState, setNavState] = useState(navBgTop);
-
-  useEffect(() => {
-    window.onscroll = () => {
-      const { innerHeight: height, scrollY: scrollPosition } = window;
-      scrollPosition > height - 100
-        ? setNavState(navBgScroll)
-        : setNavState(navBgTop);
-    };
-  }, []);
-
   return (
-    <section className="site-fixed-nav d-none d-lg-block" style={navState}>
-      <Container fluid>
-        <div className="site-wrapper">
-          <Row>
-            <Col lg={12}>
-              <ul>
-                <li>
-                  <a href="https://codehub.pro/courses/html">HTML</a>
-                </li>
-                <li>
-                  <a href="https://codehub.pro/courses/css">CSS</a>
-                </li>
-                <li>
-                  <a href="https://codehub.pro/courses/javascript">
-                    JavaScript
-                  </a>
-                </li>
-                <li>
-                  <a href="https://codehub.pro/courses/react">React</a>
-                </li>
-                <li>
-                  <a href="https://codehub.pro/courses/nodejs">Node.js</a>
-                </li>
-                <li>
-                  <a href="https://codehub.pro/courses/php">PHP</a>
-                </li>
-                <li>
-                  <a href="https://codehub.pro/courses/laravel">Laravel</a>
-                </li>
-              </ul>
-            </Col>
-          </Row>
-        </div>
-      </Container>
-    </section>
+    <ul class="site-course-nav">
+      {links.map((link) => (
+        <li>
+          <Link to={`/${link.slug}`}>{link.name}</Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
