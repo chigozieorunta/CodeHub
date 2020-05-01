e817eb1a022295079eedd9ad24d67ee8c70b09a2
chigozieorunta <chigozieorunta@yahoo.com>
2020-05-01 23:34:52 +0100

    added extra components

--- src/pages/Courses/index.js
+++ src/pages/Courses/index.js
@@ -3,22 +3,47 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import Navigation from "../../components/Navigation/Navigation";
 import MobileNavigation from "../../components/MobileNavigation/MobileNavigation";
-
-import CourseBar from "./CourseBar/CourseBar";
+import CheckBox from "../../components/CheckBox/CheckBox";
 
 import CourseBox from "../Home/CourseBox/CourseBox";
+
+import "./index.css";
 
 const Courses = () => {
   return (
     <>
       <Navigation />
       <MobileNavigation />
-      <CourseBar />
+      <section className="site-top-spacer"></section>
       <section className="section-md">
         <div className="site-wrapper">
           <Container fluid>
             <Row>
-              <Col lg={3}></Col>
+              <Col lg={3}>
+                <ul class="site-filter">
+                  <li>
+                    <CheckBox>HTML</CheckBox>
+                  </li>
+                  <li>
+                    <CheckBox>CSS</CheckBox>
+                  </li>
+                  <li>
+                    <CheckBox>JavaScript</CheckBox>
+                  </li>
+                  <li>
+                    <CheckBox>React</CheckBox>
+                  </li>
+                  <li>
+                    <CheckBox>Node.js</CheckBox>
+                  </li>
+                  <li>
+                    <CheckBox>PHP</CheckBox>
+                  </li>
+                  <li>
+                    <CheckBox>Laravel</CheckBox>
+                  </li>
+                </ul>
+              </Col>
               <Col lg={9}>
                 <Row className="row-spacer row-col-spacer">
                   <Col lg={4}>
