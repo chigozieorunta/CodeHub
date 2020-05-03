4b9bf64e386eb07e878203b73681e2420e20b9f4
chigozieorunta <chigozieorunta@yahoo.com>
2020-05-03 20:36:00 +0100

    removed prev component code

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -35,25 +35,3 @@ const Navigation = () => {
 };
 
 export default Navigation;
-
-/*import { Navbar, Nav } from "react-bootstrap";
-
-import "./Navigation.css";
-
-const Navigation = () => {
-  return (
-    <Navbar fixed="top" expand="lg">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/courses">Courses</Nav.Link>
-          <Nav.Link href="/sign-up">Sign Up</Nav.Link>
-          <Nav.Link href="/login">Login</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
-
-export default Navigation;*/
