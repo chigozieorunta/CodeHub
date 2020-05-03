0e6ec4ece15e0ab43b96472d0d00fb2ba381fa22
chigozieorunta <chigozieorunta@yahoo.com>
2020-05-03 20:30:50 +0100

    changed navigation from bootstrap style to custom style

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,42 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import "./Navigation.css";
+
+const links = [
+  {
+    name: "Home",
+    slug: "",
+  },
+  {
+    name: "Courses",
+    slug: "courses",
+  },
+  {
+    name: "Sign Up",
+    slug: "sign-up",
+  },
+  {
+    name: "Login",
+    slug: "login",
+  },
+];
+
+const Navigation = () => {
+  return (
+    <ul class="site-main-nav">
+      {links.map((link) => (
+        <li>
+          <Link to={`/${link.slug}`}>{link.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Navigation;
+
+/*import { Navbar, Nav } from "react-bootstrap";
 
 import "./Navigation.css";
 
@@ -19,4 +56,4 @@ const Navigation = () => {
   );
 };
 
-export default Navigation;
+export default Navigation;*/
